9abb13a9d1cea55dd5a41b3a9fc179442a611d47
Tobias Buschor <tobias.buschor@shwups.ch>
2019-06-18 08:31:39 +0200

    initial

---
+++ server.js
@@ -0,0 +1,26 @@
+class server {
+    constructor(options={}){
+        this.options = options;
+    }
+    async listen(req){
+        const fileName = req.url.replace(/\/$/, '');
+        var found = await serve(request, this.options.base + fileName);
+        if (!found) return false;
+        response = await serveFile(req, filePath);
+        req.respond(response);
+        return true;
+    }
+    serve(request, file){
+        const file = await open(filename);
+        const fileInfo = await stat(filename);
+        const headers = new Headers();
+        headers.set("content-length", fileInfo.len.toString());
+        headers.set("content-type", contentType(extname(filename)) || 'text/plain');
+        const res = {
+            status: 200,
+            body: file,
+            headers
+        };
+        return res;        
+    }
+}
