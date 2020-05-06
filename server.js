6d40761a6b79cf5cc22d6900b3d7f68a40115d11
Tobias Buschor <tobias.buschor@shwups.ch>
2020-05-06 23:52:36 +0200

    Update server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ export class FileServer {
         this.documentRoot = documentRoot;
         this.basePath = basePath;
     }
-    async serve(req){
-        const pathName = req.URL.pathname;
+    async serve(ctx){
+        const pathName = ctx.in.url.pathname;
         if (!pathName.startsWith(this.basePath)) return;
         const urlPart = pathName.substr(this.basePath.length);
-        var found = await fileToResponse(this.documentRoot + '/' + urlPart, req.response);
+        var found = await fileToResponse(this.documentRoot + '/' + urlPart, ctx.out);
         if (!found) return;
         return true;
     }
@@ -19,8 +19,8 @@ async function fileToResponse(path, response){
     path = path.replace(/^file:\/\//,'');
     let fileInfo = await stat(path);
     if (!fileInfo || !fileInfo.isFile) return;
-    response.header['content-length'] = fileInfo.size;
-    response.header['content-type'] = pathToContentType(path) || 'text/plain';
+    response.headers.set('content-length', fileInfo.size);
+    response.headers.set('content-type', pathToContentType(path) || 'text/plain');
     response.body = await Deno.open(path);
     return true;
 }
