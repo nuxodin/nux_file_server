4f0fb53e8982332e60121caf8ff08e0b1faf3863
Tobias Buschor <tobias.buschor@shwups.ch>
2021-05-08 20:44:31 +0200

    Update util.js

--- util.js
+++ util.js
@@ -1,4 +1,4 @@
-import { extname } from "https://deno.land/std@0.79.0/path/mod.ts";
+import { extname } from "https://deno.land/std@0.93.0/path/mod.ts";
 
 
 export function pathToContentType(path) {
